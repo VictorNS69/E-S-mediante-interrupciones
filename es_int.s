55b57e84c079615c25d4f14692c657aea8fd1a25
Víctor Nieves Sánchez <vnievess@gmail.com>
2019-03-05 13:44:15 +0100

    body of the project

---
+++ es_int.s
@@ -0,0 +1,58 @@
+* Inicializa el SP y el PC
+**************************
+        ORG     $0
+        DC.L    $8000           * Pila
+        DC.L    INICIO          * PC
+
+        ORG     $400
+
+* Definición de equivalencias
+*********************************
+
+MR1A    EQU     $effc01       * de modo A (escritura)
+MR2A    EQU     $effc01       * de modo A (2º escritura)
+SRA     EQU     $effc03       * de estado A (lectura)
+CSRA    EQU     $effc03       * de seleccion de reloj A (escritura)
+CRA     EQU     $effc05       * de control A (escritura)
+TBA     EQU     $effc07       * buffer transmision A (escritura)
+RBA     EQU     $effc07       * buffer recepcion A  (lectura)
+ACR	EQU	$effc09	      * de control auxiliar
+IMR     EQU     $effc0B       * de mascara de interrupcion A (escritura)
+ISR     EQU     $effc0B       * de estado de interrupcion A (lectura)
+
+
+**************************** INIT *************************************************************
+INIT:
+        MOVE.B          #%00010000,CRA      * Reinicia el puntero MR1
+        MOVE.B          #%00000011,MR1A     * 8 bits por caracter.
+        MOVE.B          #%00000000,MR2A     * Eco desactivado.
+        MOVE.B          #%11001100,CSRA     * Velocidad = 38400 bps.
+        MOVE.B          #%00000000,ACR      * Velocidad = 38400 bps.
+        MOVE.B          #%00000101,CRA      * Transmision y recepcion activados.
+        RTS
+**************************** FIN INIT *********************************************************
+
+**************************** LEECAR ***********************************************************
+LEECAR:	BREAK
+**************************** FIN LEECAR *******************************************************
+
+**************************** ESCCAR ***********************************************************
+ESCCAR:	BREAK
+**************************** FIN ESCCAR *******************************************************
+
+**************************** LINEA ************************************************************
+LINEA:	BREAK
+**************************** FIN LINEA ********************************************************
+
+
+**************************** PRINT ************************************************************
+PRINT:  BREAK
+**************************** FIN PRINT ********************************************************
+
+**************************** SCAN ************************************************************
+SCAN:   BREAK
+**************************** FIN SCAN ********************************************************
+
+**************************** PROGRAMA PRINCIPAL **********************************************
+INICIO: BREAK
+**************************** FIN PROGRAMA PRINCIPAL ******************************************
